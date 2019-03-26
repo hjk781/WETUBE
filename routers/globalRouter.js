58dfc5cf0966032b01375a003ba1121d968e1eea
hjk781 <hjk781@naver.com>
2019-03-26 17:37:54 +0900

    login controller

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,15 +1,17 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controller/videoController";
-import { getJoin , login, logout, postJoin } from "../controller/userControllers";
+import { getJoin , getLogin, logout, postJoin, postLogin } from "../controller/userControllers";
 const globalRouter = express.Router();
 
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
