10ec4dce601816f15a46f60af5d12faed8ade123
yeonggwan <hjk781@naver.com>
2019-03-27 16:20:16 +0900

    mongoDB part 1

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,6 +1,15 @@
 import routes from "../routes";
+import Video from "../models/Video"
 
-export const home = (req, res) => res.render("home", {pageTitle: "Home", videos});
+export const home = async(req, res) => {
+    try{
+        const videos = await Video.find({});
+        res.render("home", {pageTitle: "Home", videos});
+    } catch(error){
+        console.log(error);
+        res.render("home", {pageTitle: "Home", videos: [] });
+    }
+};
 
 export const search = (req, res) => {
     // es6 이전방식
@@ -13,16 +22,34 @@ export const search = (req, res) => {
 
 export const getUpload = (req, res) => res.render("upload", {pageTitle: "Upload"});
 
-export const postUpload = (req, res) => {
+export const postUpload = async(req, res) => {
     const {
-        body:{ file,title,description }
+        body:{ title,description },
+        file: {path}
     } = req;
+    const newVideo = await Video.create({
+        fileUrl: path,
+        title,
+        description
+    });
     //To Do: Upload and save video
-    res.redirect(routes.videoDetail(324393));
+    console.log(newVideo);
+    res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Video Detail"});
+export const videoDetail = async(req, res) => {
+    const{
+        params: {id}
+    } = req;
+    try{
+        const video = await Video.findById(id);
+        res.render("videoDetail", {pageTitle: "Video Detail", video});
+    } catch(error){
+        console.log(error);
+        res.redirect(routes.home);
+    }
+}
 
 export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "Edit Video"});
 
-export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "Delete Video"});
+export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "Delete Video"});``
