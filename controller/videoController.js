8d6e4aa03967447833f902ce90d034cc6ae29309
yeonggwan <hjk781@naver.com>
2019-03-28 11:46:31 +0900

    edit Video

--- controller/videoController.js
+++ controller/videoController.js
@@ -44,12 +44,35 @@ export const videoDetail = async(req, res) => {
     try{
         const video = await Video.findById(id);
         res.render("videoDetail", {pageTitle: "Video Detail", video});
-    } catch(error){
+    } catch (error) {
         console.log(error);
         res.redirect(routes.home);
     }
 }
 
-export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "Edit Video"});
+export const getEditVideo = async (req, res) => {
+    const {
+        params: {id}
+    } = req;
+    try{
+        const video = await Video.findById(id);
+        res.render("editVIdeo", {pageTitle: `Edit ${video.title}`, video})
+    } catch (erroe) {
+        res.redirect(routes.home)
+    }
+}
+
+export const postEditVideo = async (req, res) => {
+    const {
+        params: {id},
+        body: {title, description}
+    } = req;
+    try {
+        await Video.findOneAndUpdate({ _id: id }, {title,description});
+        res.redirect(routes.videoDetail(id));
+    } catch (error) {
+        res.redirect(routes.home)
+    }
+};
 
 export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "Delete Video"});``
