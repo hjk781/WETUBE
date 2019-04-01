8c4e55c30fc6b464549f115675a8e701fd97cb72
yeonggwan <hjk781@naver.com>
2019-04-01 22:40:21 +0900

    user profile

--- controller/userControllers.js
+++ controller/userControllers.js
@@ -41,7 +41,7 @@ export const githubLogin = passport.authenticate("github");
 
 export const githubLoginCallback = async (_, __, profile, cb) => {
   const {
-    _json: { id, avatar_url, name, email }
+    _json: { id, avatar_url: avatarUrl, name, email }
   } = profile;
   try {
     const user = await User.findOne({ email });
@@ -54,7 +54,7 @@ export const githubLoginCallback = async (_, __, profile, cb) => {
       email,
       name,
       githubId: id,
-      avatarUrl: avatar_url
+      avatarUrl
     });
     return cb(null, newUser);
   } catch (error) {
@@ -71,6 +71,11 @@ export const logout = (req, res) => {
   req.logout();
   res.redirect(routes.home);
 };
+
+export const getMe = (req, res) => {
+  res.render("userDetail", { pageTitle: "User Detail", user: req.user });
+};
+
 export const users = (req, res) => res.render("users", { pageTitle: "User" });
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
