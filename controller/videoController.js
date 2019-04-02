241713c4f3fcc06f241010b370443e58c05d3898
yeonggwan <hjk781@naver.com>
2019-04-02 15:03:07 +0900

    sucess

--- controller/videoController.js
+++ controller/videoController.js
@@ -39,10 +39,11 @@ export const postUpload = async (req, res) => {
   const newVideo = await Video.create({
     fileUrl: path,
     title,
-    description
+    description,
+    creator: req.user.id
   });
-  // To Do: Upload and save video
-  console.log(newVideo);
+  req.user.videos.push(newVideo.id);
+  req.user.save();
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
@@ -51,7 +52,8 @@ export const videoDetail = async (req, res) => {
     params: { id }
   } = req;
   try {
-    const video = await Video.findById(id);
+    const video = await Video.findById(id).populate("creator");
+    console.log(video);
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     console.log(error);
@@ -65,6 +67,7 @@ export const getEditVideo = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id);
+    if(video.creator !=== use)
     res.render("editVIdeo", { pageTitle: `Edit ${video.title}`, video });
   } catch (erroe) {
     res.redirect(routes.home);
--- models/Comment.js
+++ models/Comment.js
@@ -1,19 +1,23 @@
 import mongoose from "mongoose";
 
 const CommentSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        required: "Text URL is required"
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-    // ,
-    // video: {
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: "Video"
-    // }
+  text: {
+    type: String,
+    required: "Text URL is required"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+  // ,
+  // video: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "Video"
+  // }
 });
 
 const model = mongoose.model("Comment", CommentSchema);
